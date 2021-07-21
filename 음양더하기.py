1d578bad1c37e5c906bd1ff4ba709450f21e47de
Jenny Park <slpjenny@naver.com>
2021-07-21 13:54:12 +0900

    Create 음양더하기.py
    zip()

---
+++ 음양더하기.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    answer=0
+    
+    for a,s in zip(absolutes,signs):
+        if s:
+            answer+=a
+        else:
+            answer-=a
+    
+    return answer
